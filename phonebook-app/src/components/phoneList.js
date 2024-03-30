efb4929f5e8b653ee65d17d98619532c1f0b3700
hayeonbae7 <baehayeon@naver.com>
2024-03-30 23:50:37 +0900

    add: UI

---
+++ phonebook-app/src/components/phoneList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function PhoneList() {
+  return (
+    <div>
+        <div>배하연</div>
+        <div>배하연</div>
+        <div>배하연</div>
+        <div>배하연</div>
+    </div>
+  )
+}
+
+export default PhoneList
